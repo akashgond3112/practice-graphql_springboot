b40eb6ec3e23dd2226996e3a80a763d983233829
agond <akashgond311@gmail.com>
2024-08-24 13:00:45 +0100

    Commit-24/08/2024

--- spring_graphql/src/main/java/com/graphql/spring_graphql/Author.java
+++ spring_graphql/src/main/java/com/graphql/spring_graphql/Author.java
@@ -6,7 +6,10 @@ import java.util.Optional;
 
 public record Author(Integer id, String name) {
 
-	public static List<Author> authors = Arrays.asList(new Author(1, "Akash"), new Author(2, "Vikash"));
+	public static List<Author> authors = Arrays.asList(
+			new Author(1, "Akash"),
+			new Author(2, "Vikash")
+	);
 
 	public static Optional<Author> getAuthorById(Integer id) {
 		return authors.stream().filter(b -> b.id.equals(id)).findFirst();
--- spring_graphql/src/main/java/com/graphql/spring_graphql/BookController.java
+++ spring_graphql/src/main/java/com/graphql/spring_graphql/BookController.java
@@ -2,9 +2,11 @@ package com.graphql.spring_graphql;
 
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class BookController {
@@ -17,5 +19,10 @@ public class BookController {
 	@QueryMapping
 	public Book getBookById(@Argument Integer id) {
 		return Book.getBookById(id);
+	}
+
+	@SchemaMapping
+	public Optional<Author> author(Book book) {
+		return Author.getAuthorById(book.authorId());
 	}
 }
